8739e77960a1f67f17c510df1cc92efc1bc76852
Chase Moskal <chasemoskal@gmail.com>
2019-08-11 11:19:24 -0700

    fix compat with ms edge

--- source/components/menu-system.js
+++ source/components/menu-system.js
@@ -2,6 +2,7 @@
 import {MenuDisplay} from "./menu-display.js"
 import {Component, html, css} from "../toolbox/component.js"
 import {makeScrollMarmot} from "../toolbox/make-scroll-marmot.js"
+import {getAssignedElements} from "../toolbox/get-assigned-elements.js"
 
 const _scrollTop = Symbol("scrollTop")
 const _shadowRoot = Symbol("shadowRoot")
@@ -13,6 +14,99 @@ const _updateScrollPoint = Symbol("updateScrollPoint")
 const _handleBlanketClick = Symbol("handleBlanketClick")
 
 export class MenuSystem extends Component {
+
+	static get properties() {
+		return {
+			["initially-hidden"]: {type: Boolean, reflect: true},
+			theme: {type: String, reflect: true},
+			lefty: {type: Boolean, reflect: true},
+			active: {type: Boolean, reflect: true},
+			sticky: {type: Boolean, reflect: true},
+			[_scrollTop]: {type: Number},
+			[_activeIndex]: {type: String}
+		}
+	}
+
+	constructor() {
+		super()
+
+		this.theme = "concrete"
+		this.active = false
+		this.sticky = false
+		this.lefty = false
+
+		this[_activeIndex] = undefined
+		this[_scrollTop] = 0
+		this[_scrollMarmot] = undefined
+
+		this[_updateScrollPoint] = scrollPoint => {
+			if (!this.active) this[_scrollTop] = scrollPoint
+		}
+
+		this[_handleBlanketClick] = () => {
+			this[_toggleIndex](this[_activeIndex])
+		}
+
+		this[_toggleIndex] = index => {
+			this[_activeIndex] = index === this[_activeIndex]
+				? undefined
+				: index
+			this.active = this[_activeIndex] !== undefined
+		}
+
+		this[_getMenuDisplays] = () => {
+			const slot = this[_shadowRoot].querySelector("slot")
+			return getAssignedElements(slot).filter(
+				element => element instanceof MenuDisplay
+			)
+		}
+	}
+
+	createRenderRoot() {
+		this[_shadowRoot] = this.attachShadow({mode: "closed"})
+		this[_shadowRoot].addEventListener("slotchange", () => this.requestUpdate())
+		return this[_shadowRoot]
+	}
+
+	_initScrollMarmot() {
+		if (this.sticky && !this[_scrollMarmot])
+			this[_scrollMarmot] = makeScrollMarmot({
+				onScrollUpdate: this[_updateScrollPoint]
+			})
+	}
+
+	_disposeScrollMarmot() {
+		if (this[_scrollMarmot]) {
+			this[_scrollMarmot].dispose()
+			this[_scrollMarmot] = null
+		}
+	}
+
+	connectedCallback() {
+		super.connectedCallback()
+		if (this.isConnected) this._initScrollMarmot()
+	}
+
+	disconnectedCallback() {
+		super.disconnectedCallback()
+		this._disposeScrollMarmot()
+	}
+
+	updated() {
+		this._initScrollMarmot()
+		if (!this.sticky) this._disposeScrollMarmot()
+
+		this[_getMenuDisplays]().forEach((display, index) => {
+			display.theme = this.theme
+			display.lefty = this.lefty
+			display.toggle = () => this[_toggleIndex](index)
+			display.open = index === this[_activeIndex]
+		})
+	}
+
+	firstUpdated() {
+		this["initially-hidden"] = false
+	}
 
 	static get styles() {
 		return css`
@@ -93,100 +187,6 @@ export class MenuSystem extends Component {
 				flex: 0 0 auto;
 			}
 		`
-	}
-
-	static get properties() {
-		return {
-			["initially-hidden"]: {type: Boolean, reflect: true},
-			theme: {type: String, reflect: true},
-			lefty: {type: Boolean, reflect: true},
-			active: {type: Boolean, reflect: true},
-			sticky: {type: Boolean, reflect: true},
-			[_scrollTop]: {type: Number},
-			[_activeIndex]: {type: String}
-		}
-	}
-
-	constructor() {
-		super()
-
-		this.theme = "concrete"
-		this.active = false
-		this.sticky = false
-		this.lefty = false
-
-		this[_activeIndex] = undefined
-		this[_scrollTop] = 0
-		this[_scrollMarmot] = undefined
-
-		this[_updateScrollPoint] = scrollPoint => {
-			if (!this.active) this[_scrollTop] = scrollPoint
-		}
-
-		this[_handleBlanketClick] = () => {
-			this[_toggleIndex](this[_activeIndex])
-		}
-
-		this[_toggleIndex] = index => {
-			this[_activeIndex] = index === this[_activeIndex]
-				? undefined
-				: index
-			this.active = this[_activeIndex] !== undefined
-		}
-
-		this[_getMenuDisplays] = () => {
-			const slot = this[_shadowRoot].querySelector("slot")
-			const elements = Array.from(slot.assignedElements())
-			return elements.filter(
-				element => element instanceof MenuDisplay
-			)
-		}
-	}
-
-	createRenderRoot() {
-		this[_shadowRoot] = this.attachShadow({mode: "closed"})
-		this[_shadowRoot].addEventListener("slotchange", () => this.requestUpdate())
-		return this[_shadowRoot]
-	}
-
-	_initScrollMarmot() {
-		if (this.sticky && !this[_scrollMarmot])
-			this[_scrollMarmot] = makeScrollMarmot({
-				onScrollUpdate: this[_updateScrollPoint]
-			})
-	}
-
-	_disposeScrollMarmot() {
-		if (this[_scrollMarmot]) {
-			this[_scrollMarmot].dispose()
-			this[_scrollMarmot] = null
-		}
-	}
-
-	connectedCallback() {
-		super.connectedCallback()
-		if (this.isConnected) this._initScrollMarmot()
-	}
-
-	disconnectedCallback() {
-		super.disconnectedCallback()
-		this._disposeScrollMarmot()
-	}
-
-	updated() {
-		this._initScrollMarmot()
-		if (!this.sticky) this._disposeScrollMarmot()
-
-		this[_getMenuDisplays]().forEach((display, index) => {
-			display.theme = this.theme
-			display.lefty = this.lefty
-			display.toggle = () => this[_toggleIndex](index)
-			display.open = index === this[_activeIndex]
-		})
-	}
-
-	firstUpdated() {
-		this["initially-hidden"] = false
 	}
 
 	render() {
